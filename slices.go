329678fd45a9be48cad68b6a68000ebb682929e1
Emil Persson <emil.g.persson@gmail.com>
2015-03-17 15:34:10 +0100

    added fibonacci & slices

--- slices.go
+++ slices.go
@@ -2,24 +2,36 @@ package main
 
 import (
 	"golang.org/x/tour/pic"
+	"math"
 )
 
 func Pic(dx, dy int) [][]uint8 {
-	var z [][]uint8
-	var c []uint8
-	x := 2.0
-	y := 3.0
-	for i := 0; i < dx; i++ {
-		c = append(c, uint8((x*y)/2))
-		y++
-		x++
+	// Skapar först en slice med längden dy
+	a := make([][]uint8, dy)
+	for i := 0; i < dy; i++ { // Fyller sedan a med slices av längden dx
+		a[i] = make([]uint8, dx)
+	}
+	x := 3.0
+	y := 5.0
+
+	for i := 0; i < dy; i++ {
+		for j := 0; j < dx; j++ {
+			switch {
+			case j%5 == 0:
+				a[i][j] = uint8(x * y)
+			case j%3 == 0:
+				a[i][j] = uint8((x + y) / 2)
+			case j%2 == 0:
+				a[i][j] = uint8(math.Pow(x, y))
+			default:
+				a[i][j] = 50
+
+			}
+		}
+
 	}
 
-	for a := 0; a < dy; a++ {
-		z = append(z, c)
-	}
-
-	return z
+	return a
 }
 
 func main() {
