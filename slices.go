d351c463f6d485151a8905f8c221aa268bc90d2b
Emil Persson <emil.g.persson@gmail.com>
2015-03-17 16:03:47 +0100

    update

--- slices.go
+++ slices.go
@@ -11,20 +11,17 @@ func Pic(dx, dy int) [][]uint8 {
 	for i := 0; i < dy; i++ { // Fyller sedan a med slices av längden dx
 		a[i] = make([]uint8, dx)
 	}
-	x := 3.0
-	y := 5.0
-
 	for i := 0; i < dy; i++ {
 		for j := 0; j < dx; j++ {
 			switch {
 			case j%5 == 0:
-				a[i][j] = uint8(x * y)
-			case j%3 == 0:
-				a[i][j] = uint8((x + y) / 2)
+				a[i][j] = uint8(i * j)
 			case j%2 == 0:
+				y := 2.0
+				x := 10.0
 				a[i][j] = uint8(math.Pow(x, y))
 			default:
-				a[i][j] = 50
+				a[i][j] = uint8((i + j) / 2)
 
 			}
 		}
